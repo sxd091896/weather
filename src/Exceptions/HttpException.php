50758db0f363f844c6ebf6cd15741ff4d581d862
sundong <55918802@qq.com>
2019-10-29 09:47:47 +0800

    fasd

---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sun\Weather\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sun\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
